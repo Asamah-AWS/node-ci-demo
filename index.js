35ea38d51d0821ed928b270b8e80662a93ecb361
islndrr <160671788+islndrr@users.noreply.github.com>
2025-06-29 21:15:43 +0200

    added covrage

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+// index.js
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// Export so tests can import them
+module.exports = { add, subtract };
